8be9c4d5b13b4f472ebf03888da8f0db0091177a
Adam Schwartz <adam@adamschwartz.io>
2014-06-03 17:25:07 -0500

    added function to change music player to Spotify

--- helm-itunes.el
+++ helm-itunes.el
@@ -10,48 +10,67 @@
 ;;
 ;; A search & play interface for iTunes.
 ;;
-;; Currently only supports OSX.
+;; There should be a simple way to get the songs to play
+;; with Spotify instead of iTunes.
+;;
+;; Currently only supports OS X.
 ;;
 
 ;;; Code:
 
-;; (defvar track-uri "spotify:local:Beastie+Boys:Ill Communication:Sure+Shot:123")
+;; (defvar track-uri nil)
+;; (setq "spotify:local:Beastie+Boys:Ill Communication:Sure+Shot:123")
 ;; (shell-command (format "osascript -e 'tell application %S to play track %S'" "Spotify" track-uri))
 
 ;; Either an artist, album, or song name to search for.
-(defvar search-pattern "Sure Shot")
+(defvar search-pattern nil)
+(setq search-pattern "Sure Shot")
+
+
+;; Change the music player to Spotify
+(defvar music-player nil "initialize music-player variable")
+
+(defun helm-itunes-player (player)
+  "Choose Spotify or iTunes as the music player"
+
+  (interactive "sPlay music through Spotify? (y/n): ")
+  (if (or (equal (downcase player) "y")
+          (equal (downcase player) "yes"))
+      (progn      (message "Player set to Spotify")
+                  (setq music-player t))
+    (progn (message "Player set to iTunes"))
+    (setq music-player nil)))
+
 
 ;; AppleScript that searches iTunes for songs.
-(defvar search-script
+(defun search-script (pattern)
   (format "-- toggle a variable if iTunes was running before this script was run.
-    -- if application \"iTunes\" is running then
-    --	set irun to true
-    -- else
-    --	set irun to false
-    -- end if
-    
-    set pattern to %S
-    
-    set matches to {}
-    tell application \"iTunes\"
-	repeat with t in (file tracks whose artist contains pattern ¬
-		or album contains pattern or name contains pattern)
-		set matches to matches & ¬
-			{{artist of t as string, album of t as string, ¬
-				(name of t as string) & \"pattern-match-end\"}}
-	end repeat
-	
-    end tell
-    
-    -- If you never had iTunes open, then quit after performing the search, 
-    -- otherwise leave it open.
-    
-    -- if not irun then
-    -- 	tell application \"iTunes\" to quit
-    -- end if
-    
-    -- Return the list of songs that matched the search
-    return matches" search-pattern))
+if application \"iTunes\" is running then
+        set irun to true
+else
+        set irun to false
+end if
+
+set pattern to %S
+
+set matches to {}
+tell application \"iTunes\"
+        repeat with t in (file tracks whose artist contains pattern ¬
+                or album contains pattern or name contains pattern)
+                set matches to matches & ¬
+                        {{artist of t as string, album of t as string, ¬
+                                (name of t as string) & \"pattern-match-end\"}}
+        end repeat
+
+end tell
+
+-- If you never had iTunes open, then quit after performing the search, otherwise leave it open.
+if not irun then
+        tell application \"iTunes\" to quit
+end if
+
+-- Return the list of songs that matched the search
+return matches" pattern))
 
 
 ;; Return a list of matches from the results of running the AppleScript.
@@ -60,8 +79,11 @@
 ;; a list of artist, album, and name.
 ;; Finally, concatenate the list items into a single string.
 
-(mapc (lambda (song-list)
-        (split-string song-list "\\,\s")) (split-string 
-                                           (shell-command-to-string 
-                                            (format "osascript -e %S" search-script)) 
-                                           "pattern-match-end,\s\\|pattern-match-end"))
+(defun get-song-list (pattern)
+  (mapc (lambda (song-list)
+          (split-string song-list "\\,\s")) (split-string
+                                             (shell-command-to-string
+                                              (format "osascript -e %S" (search-script pattern)))
+                                             "pattern-match-end,\s\\|pattern-match-end")))
+
+(get-song-list search-pattern)

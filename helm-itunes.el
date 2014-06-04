3340191cca8e123eb14f5a58fb0025593dc26195
Adam Schwartz <adam@adamschwartz.io>
2014-06-03 20:19:46 -0500

    Use helm to play tracks in either Spotify or iTunes. However, custom searches cause Emacs to crash

--- helm-itunes.el
+++ helm-itunes.el
@@ -18,6 +18,8 @@
 
 ;;; Code:
 
+(require 'helm)
+
 ;; (defvar track-uri nil)
 ;; (setq "spotify:local:Beastie+Boys:Ill Communication:Sure+Shot:123")
 ;; (shell-command (format "osascript -e 'tell application %S to play track %S'" "Spotify" track-uri))
@@ -28,7 +30,8 @@
 
 
 ;; Change the music player to Spotify
-(defvar music-player nil "initialize music-player variable")
+(defvar music-player nil "initialize spotify-player variable")
+(setq-default music-player "itunes")
 
 (defun helm-itunes-player (player)
   "Choose Spotify or iTunes as the music player"
@@ -37,9 +40,9 @@
   (if (or (equal (downcase player) "y")
           (equal (downcase player) "yes"))
       (progn      (message "Player set to Spotify")
-                  (setq music-player t))
+                  (setq music-player "spotify"))
     (progn (message "Player set to iTunes"))
-    (setq music-player nil)))
+    (setq music-player "itunes")))
 
 
 ;; AppleScript that searches iTunes for songs.
@@ -80,10 +83,61 @@ return matches" pattern))
 ;; Finally, concatenate the list items into a single string.
 
 (defun get-song-list (pattern)
-  (mapc (lambda (song-list)
-          (split-string song-list "\\,\s")) (split-string
-                                             (shell-command-to-string
-                                              (format "osascript -e %S" (search-script pattern)))
-                                             "pattern-match-end,\s\\|pattern-match-end")))
+  (mapcar (lambda (song-list)
+            (split-string song-list "\\,\s")) (split-string
+                                               (shell-command-to-string
+                                                (format "osascript -e %S" (search-script pattern)))
+                                               "pattern-match-end,\s\\|pattern-match-end")))
 
 (get-song-list search-pattern)
+
+
+(defun itunes-format-track (track)
+  "Given a track, return a formatted string to display"
+  (let ((song (nth 2 track))
+        (artist (nth 0 track))
+        (album (nth 1 track)))
+    (format "%S\n %S\n %S" song artist album)))
+
+
+(itunes-format-track '("Artist" "Album" "Song"))
+
+(defun spotify-format-track (track)
+  "Return a Spotify compatible string to play"
+  (let ((song (nth 2 track))
+        (artist (nth 0 track))
+        (album (nth 1 track)))
+    (format "spotify:local:%s:%s:%s:123" artist album song)))
+
+(spotify-format-track '("Artist" "Album" "Song"))
+
+;; Helm Functions
+
+(defun helm-itunes-search ()
+  (mapcar (lambda (track)
+            (cons (itunes-format-track track)
+                  (if (equal music-player "spotify")
+                      (spotify-format-track track)
+                    (nth 2 track))))
+          (get-song-list search-pattern)))
+
+
+(defun helm-itunes-play-track (track)
+  (shell-command (format "osascript -e 'tell application %S to play track %S'" music-player track)))
+
+
+(defvar helm-source-itunes-track-search
+  '((name . "iTunes Search")
+    (volatile)
+    (delayed)
+    (multiline)
+                                        ;(requires-pattern . 2)
+    (candidates-process . helm-itunes-search)
+    (action . (("Play Track" . helm-itunes-play-track)))))
+
+
+(defun helm-itunes ()
+  "Bring up a Spotify search interface in helm."
+  (interactive)
+  (helm :sources '(helm-source-itunes-track-search)
+        :buffer "*helm-itunes*"))
